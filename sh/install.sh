4136fdf7d94417142ef7f9e6c7eb9b72337d0929
Jérémie Litzler <contact@jeremielitzler.net>
2016-06-07 06:45:17 +0200

    sh scripts to help use the app

---
+++ sh/install.sh
@@ -0,0 +1,12 @@
+git clone https://github.com/Puzzlout/TrekToursFlyer.git
+cd TrekToursFlyer
+composer install
+composer update
+echo "require a mysql root user with no password"
+php bin/console doctrine:database:create
+php bin/console doctrine:schema:update --force
+php bin/console doctrine:schema:update --dump-sql
+php bin/console doctrine:schema:update --force
+php bin/console assetic:dump --env=prod --no-debug
+php bin/console asset:install --symlink
+php bin/console asset:install --symlink
