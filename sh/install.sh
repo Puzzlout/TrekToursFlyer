81e75db15fc141e84c2383c2efe3489df02cda2e
Jérémie Litzler <contact@jeremielitzler.net>
2016-06-08 06:56:51 +0200

    #11
    Adding bower to sh scripts.

--- sh/install.sh
+++ sh/install.sh
@@ -1,12 +1,15 @@
-#git clone https://github.com/Puzzlout/TrekToursFlyer.git
-#cd TrekToursFlyer
+# git clone https://github.com/Puzzlout/TrekToursFlyer.git
+# cd TrekToursFlyer
+echo "Run composer..."
 composer install
 composer update
-echo "require a mysql root user with no password"
+echo "Run bower..."
+bower install
+bower update
+echo "IMPORTANT: Require a mysql root user with no password"
 php bin/console doctrine:database:create --if-not-exists
 php bin/console doctrine:schema:update --force
 php bin/console doctrine:schema:update --dump-sql
 php bin/console doctrine:schema:update --force
 php bin/console assetic:dump --env=prod --no-debug
-php bin/console asset:install --symlink
 php bin/console asset:install --symlink
