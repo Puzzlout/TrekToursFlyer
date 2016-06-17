ef9434ad5d220ff0073ccacf189398dbf5649101
Jérémie Litzler <contact@jeremielitzler.net>
2016-06-17 07:20:41 +0200

    #45

--- src/AdminBundle/Controller/AdminController.php
+++ src/AdminBundle/Controller/AdminController.php
@@ -3,6 +3,7 @@
 namespace AdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
 
 class AdminController extends Controller
