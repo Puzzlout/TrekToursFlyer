9ff12b2d9f558dfe09755769a2c41f9fd217351f
Damir <damirius@gmail.com>
2016-06-21 23:12:43 +0200

    #106 Added CI rest client bundle and created call to post customerinforequests api endpoint, together with unit tests for formtype

--- src/AppBundle/Controller/ContactController.php
+++ src/AppBundle/Controller/ContactController.php
@@ -4,7 +4,9 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use AppBundle\Form\Type\CustomerInfoRequestType;
+use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 class ContactController extends Controller
 {
@@ -19,7 +21,25 @@ class ContactController extends Controller
         $form->handleRequest($request);
         if($form->isSubmitted() && $form->isValid())
         {
-            //TODO: Implement API call
+            $apiUrl = $this->getParameter('api_url');
+            $apiFormat = $this->getParameter('api_format');
+            $restClient = $this->container->get('circle.restclient');
+            $formData = $form->getData();
+            $formData['status'] = 'TBP';
+            try {
+                $response = $restClient->post($apiUrl.'/customerinforequests'.$apiFormat, json_encode($formData));
+                if($response->getStatusCode() != 201)
+                    throw new \Exception();
+
+                unset($form);
+                $form = $this->createForm(CustomerInfoRequestType::class, null, array(
+                    'action' => $this->generateUrl('contact'),
+                    'method' => 'POST'
+                ));
+                $this->addFlash('cri_success', 'Thank You for your inquiry! We will contact you soon.');
+            } catch (\Exception $e) {
+                $form->addError(new FormError('API error'));
+            }
         }
         return $this->render("AppBundle:contact:show.html.twig", array(
             'form' => $form->createView()
