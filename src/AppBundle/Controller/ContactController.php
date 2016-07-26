c4d129935a46936fdd801a4b17612af46613ed5d
Damir <damirius@gmail.com>
2016-07-26 16:01:38 +0200

    #166 CRI template form rework, reCaptcha implemented on client side with checks

--- src/AppBundle/Controller/ContactController.php
+++ src/AppBundle/Controller/ContactController.php
@@ -22,128 +22,147 @@ class ContactController extends Controller
         $form->handleRequest($request);
         if($form->isSubmitted() && $form->isValid())
         {
-            $apiUrl = $this->getParameter('api_url');
-            $apiFormat = $this->getParameter('api_format');
             $restClient = $this->container->get('circle.restclient');
-            $formData = $form->getData();
-            $formData['status'] = 'TBP';
-            $response = $restClient->post($apiUrl.'/customerinforequests'.$apiFormat, json_encode($formData));
-            $customerInfoRequest = json_decode($response->getContent());
-            if($response->getStatusCode() == 201 && $customerInfoRequest->id) {
-                //send emails
-                $noReplyAddress = $this->container->getParameter('mail_noreply_address');
-                $contactAddress = $this->container->getParameter('mail_contact_address');
-                $adminLanguage = $this->container->getParameter('mail_admin_language');
-                $adminMessageSent = $clientMessageSent = 0;
-                $adminMessage = \Swift_Message::newInstance()
-                    ->setSubject($this->get('translator')->trans(
-                        'admin_subject',
-                        [],
-                        'email',
-                        $adminLanguage
-                        ))
-                    ->setFrom($noReplyAddress)
-                    ->setTo($contactAddress)
-                    ->setBody(
-                        $this->renderView(
-                            'AppBundle:emails:cri_admin.html.twig',
-                            [
-                                'first_name' => $formData['first_name'],
-                                'last_name' => $formData['last_name'],
-                                'message' => $formData['message'],
-                                'phone_number' => $formData['phone_number'],
-                                'email' => $formData['email'],
-                                'admin_email_language' => $adminLanguage
-                            ]
-                        ),
-                        'text/html'
-                    )->addPart(
-                        $this->renderView(
-                            'AppBundle:emails:cri_admin.txt.twig',
-                            [
-                                'first_name' => $formData['first_name'],
-                                'last_name' => $formData['last_name'],
-                                'message' => $formData['message'],
-                                'phone_number' => $formData['phone_number'],
-                                'email' => $formData['email'],
-                                'admin_email_language' => $adminLanguage
-                            ]
-                        ),
-                        'text/plain'
-                    )
-                ;
-                if($noReplyAddress && $contactAddress) {
-                    try {
-                        $adminMessageSent = $this->get('mailer')->send($adminMessage);
-                    } catch (\Exception $e) {
-                        $adminMessageSent = 0;
-                    }
-                }
-
-                if($formData['send_copy_to_client'] == 1) {
-                    $clientMessage = \Swift_Message::newInstance()
+            $captchaPayload = [
+                "secret" => $this->getParameter('recaptcha_secret'),
+                "response" => $request->request->get('g-recaptcha-response')
+            ];
+            $captchaJsonResponse = $restClient->post('https://www.google.com/recaptcha/api/siteverify',
+                http_build_query($captchaPayload),
+                [
+                    CURLOPT_SSL_VERIFYPEER => false,
+                    CURLOPT_HTTPHEADER => [ 'Content-Type: application/x-www-form-urlencoded' ]
+                ]
+            );
+            $captchaResponse = json_decode($captchaJsonResponse->getContent());
+            if(json_last_error() === 0 && $captchaResponse->success === true )
+            {
+                $apiUrl = $this->getParameter('api_url');
+                $apiFormat = $this->getParameter('api_format');
+                $formData = $form->getData();
+                $formData['status'] = 'TBP';
+                $response = $restClient->post($apiUrl.'/customerinforequests'.$apiFormat, json_encode($formData));
+                $customerInfoRequest = json_decode($response->getContent());
+                if($response->getStatusCode() == 201 && $customerInfoRequest->id) {
+                    //send emails
+                    $noReplyAddress = $this->container->getParameter('mail_noreply_address');
+                    $contactAddress = $this->container->getParameter('mail_contact_address');
+                    $adminLanguage = $this->container->getParameter('mail_admin_language');
+                    $adminMessageSent = $clientMessageSent = 0;
+                    $adminMessage = \Swift_Message::newInstance()
                         ->setSubject($this->get('translator')->trans(
-                            'client_subject',
+                            'admin_subject',
                             [],
-                            'email'
+                            'email',
+                            $adminLanguage
                         ))
                         ->setFrom($noReplyAddress)
-                        ->setTo($formData['email'])
+                        ->setTo($contactAddress)
                         ->setBody(
                             $this->renderView(
-                                'AppBundle:emails:cri_client.html.twig',
+                                'AppBundle:emails:cri_admin.html.twig',
                                 [
                                     'first_name' => $formData['first_name'],
                                     'last_name' => $formData['last_name'],
-                                    'message' => $formData['message']
+                                    'message' => $formData['message'],
+                                    'phone_number' => $formData['phone_number'],
+                                    'email' => $formData['email'],
+                                    'admin_email_language' => $adminLanguage
                                 ]
                             ),
                             'text/html'
                         )->addPart(
                             $this->renderView(
-                                'AppBundle:emails:cri_client.txt.twig',
+                                'AppBundle:emails:cri_admin.txt.twig',
                                 [
                                     'first_name' => $formData['first_name'],
                                     'last_name' => $formData['last_name'],
-                                    'message' => $formData['message']
+                                    'message' => $formData['message'],
+                                    'phone_number' => $formData['phone_number'],
+                                    'email' => $formData['email'],
+                                    'admin_email_language' => $adminLanguage
                                 ]
                             ),
                             'text/plain'
                         )
                     ;
-                    if($noReplyAddress) {
+                    if($noReplyAddress && $contactAddress) {
                         try {
-                            $clientMessageSent = $this->get('mailer')->send($clientMessage);
+                            $adminMessageSent = $this->get('mailer')->send($adminMessage);
                         } catch (\Exception $e) {
-                            $clientMessageSent = 0;
+                            $adminMessageSent = 0;
                         }
                     }
-                }
 
-                if($adminMessageSent != 0 || ($formData['send_copy_to_client'] == 1 && $clientMessageSent != 0)) {
-                    $adminEmailSent = ($adminMessageSent != 0) ? 1 : 0;
-                    $clientEmailSent = ($clientMessageSent != 0) ? 1 : 0;
-                    $response = $restClient->patch(
-                        $apiUrl.'/customerinforequests/'.$customerInfoRequest->id.'/sentemails'.$apiFormat,
-                        json_encode([
-                            'admin_email_sent' => $adminEmailSent,
-                            'client_email_sent' => $clientEmailSent
-                        ]));
-                }
+                    if($formData['send_copy_to_client'] == 1) {
+                        $clientMessage = \Swift_Message::newInstance()
+                            ->setSubject($this->get('translator')->trans(
+                                'client_subject',
+                                [],
+                                'email'
+                            ))
+                            ->setFrom($noReplyAddress)
+                            ->setTo($formData['email'])
+                            ->setBody(
+                                $this->renderView(
+                                    'AppBundle:emails:cri_client.html.twig',
+                                    [
+                                        'first_name' => $formData['first_name'],
+                                        'last_name' => $formData['last_name'],
+                                        'message' => $formData['message']
+                                    ]
+                                ),
+                                'text/html'
+                            )->addPart(
+                                $this->renderView(
+                                    'AppBundle:emails:cri_client.txt.twig',
+                                    [
+                                        'first_name' => $formData['first_name'],
+                                        'last_name' => $formData['last_name'],
+                                        'message' => $formData['message']
+                                    ]
+                                ),
+                                'text/plain'
+                            )
+                        ;
+                        if($noReplyAddress) {
+                            try {
+                                $clientMessageSent = $this->get('mailer')->send($clientMessage);
+                            } catch (\Exception $e) {
+                                $clientMessageSent = 0;
+                            }
+                        }
+                    }
 
-                unset($form);
-                $form = $this->createForm(CustomerInfoRequestType::class, null, array(
-                    'translation_domain' => 'contact',
-                    'action' => $this->generateUrl('contact'),
-                    'method' => 'POST'
-                ));
-                $this->addFlash('cri_success', $this->get('translator')->trans('div_success', [], 'contact'));
+                    if($adminMessageSent != 0 || ($formData['send_copy_to_client'] == 1 && $clientMessageSent != 0)) {
+                        $adminEmailSent = ($adminMessageSent != 0) ? 1 : 0;
+                        $clientEmailSent = ($clientMessageSent != 0) ? 1 : 0;
+                        $response = $restClient->patch(
+                            $apiUrl.'/customerinforequests/'.$customerInfoRequest->id.'/sentemails'.$apiFormat,
+                            json_encode([
+                                'admin_email_sent' => $adminEmailSent,
+                                'client_email_sent' => $clientEmailSent
+                            ]));
+                    }
+
+                    unset($form);
+                    $form = $this->createForm(CustomerInfoRequestType::class, null, array(
+                        'translation_domain' => 'contact',
+                        'action' => $this->generateUrl('contact'),
+                        'method' => 'POST'
+                    ));
+                    $this->addFlash('cri_success', $this->get('translator')->trans('div_success', [], 'contact'));
+                } else {
+                    $form->addError(new FormError('API error'));
+                }
             } else {
-                $form->addError(new FormError('API error'));
+                $form->addError(new FormError('reCaptcha error'));
             }
+
         }
         return $this->render("AppBundle:contact:show.html.twig", array(
-            'form' => $form->createView()
+            'form' => $form->createView(),
+            'recaptcha_key' => $this->getParameter('recaptcha_key')
         ));
     }
 }
