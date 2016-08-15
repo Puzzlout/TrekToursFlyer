973f791b49012aadfabe62a110098c3ab3821094
Damir <damirius@gmail.com>
2016-08-15 17:50:57 +0200

    #189 Commit message export

--- tagrelease.sh
+++ tagrelease.sh
@@ -8,5 +8,5 @@ git config --global user.name "Travis CI"
 git tag $1 -a -m "Generated tag $1 from TravisCI build $TRAVIS_BUILD_NUMBER"
 git push origin $GIT_TAG
 else
-  echo "Not a release"
+  echo "Not a release: $1"
 fi
