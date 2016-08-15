0981ad80879ea46888defc67f9fe3ef1d0f89bce
Damir <damirius@gmail.com>
2016-08-15 17:35:01 +0200

    #189 Changes to auto release script

---
+++ tagrelease.sh
@@ -0,0 +1,11 @@
+# $1: git tag
+if [[ $1 =~ ^[0-9]+\.[0-9]+\.[0-9]+]]
+then
+echo "Release $1"
+git config --global user.email "builds@travis-ci.com"
+git config --global user.name "Travis CI"
+git tag $1 -a -m "Generated tag $1 from TravisCI build $TRAVIS_BUILD_NUMBER"
+git push origin $GIT_TAG
+else
+  echo "Not a release"
+fi
