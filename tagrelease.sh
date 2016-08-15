6d4a305a8835477b7d2c342814ac4f7e59a50397
Damir <damirius@gmail.com>
2016-08-15 18:51:17 +0200

    #189 Remove test from condition

--- tagrelease.sh
+++ tagrelease.sh
@@ -8,8 +8,4 @@ if [  $? -eq 0  ] ;then
   git push origin $GIT_TAG
 else
   echo "Not a release: $1"
-  git config --global user.email "builds@travis-ci.com"
-  git config --global user.name "Travis CI"
-  git tag $1 -a -m "Generated tag $1 from TravisCI build $TRAVIS_BUILD_NUMBER"
-  git push origin $GIT_TAG
 fi
