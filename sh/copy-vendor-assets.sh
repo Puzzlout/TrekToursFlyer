c0b596c789036df8fb0c53390275c79f37ce7353
Jérémie Litzler <contact@jeremielitzler.net>
2017-03-07 06:27:19 +0100

    #264

--- sh/copy-vendor-assets.sh
+++ sh/copy-vendor-assets.sh
@@ -1,5 +1,8 @@
 # Copy the vendor assets retrieve with Bower to the AppBundle resources.
 # This is necessary in order to install without Bower.
+#!/usr/bin/env bash
 
-rm -R src/AppBundle/Resources/public/vendor/*
-cp -R web/assets/vendor/* src/AppBundle/Resources/public/vendor/
+cp web/assets/vendor/bootstrap/dist/css/bootstrap.min.css 	src/AppBundle/Resources/public/vendor/bootstrap.min.css
+cp web/assets/vendor/font-awesome/css/font-awesome.min.css 	src/AppBundle/Resources/public/vendor/font-awesome.min.css
+cp web/assets/vendor/jquery/dist/jquery.min.js 				src/AppBundle/Resources/public/vendor/jquery.min.js
+cp web/assets/vendor/bootstrap/dist/js/bootstrap.min.js 	src/AppBundle/Resources/public/vendor/bootstrap.min.js
